ed1031d241227e6d1ba97114055824a39c4934bc
BeBe <myomyatthu123321@gmail.com>
2022-10-12 18:40:25 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <h1>Hello</h1>
+  <div v-if="showModal"> 
+    <Modal :header="header" content="welcome user" @close="showModal=false" />
+  </div>
+ 
+  <button @click="showModal=true">open modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal-m.vue"
+export default {
+  data(){
+    return{
+      header:'Facebook',
+      title:"you donno js",
+      showModal:false,
+    }
+  },
+  method:{
+    
+  },
+  components:{
+    Modal
+  }
+}
+</script>
+
+<style scoped>
+ h1{
+  color:blue;
+ }
+</style>
---
+++ src/components/Modal-m.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{success:theme==='success',danger:theme==='delete'}">
+      <h1>{{header}}</h1>
+      <p>{{content}}</p>
+     
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props:['header','content','theme',],
+  method:{
+    closeModal(){
+      this.$emit("close")
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .backdrop{
+    position: fixed;
+    top:0;
+    background: rgba(0, 0, 0, 0.5);
+    width : 100%;
+    height : 100%;
+    }
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin:100px auto;
+        background-color: white;
+        border-radius: 10px;
+    }
+    .success{
+      background-color: green;
+      color: beige;
+    }
+    .delete{
+      background-color: brown;
+      color:beige;
+    }
+</style>
